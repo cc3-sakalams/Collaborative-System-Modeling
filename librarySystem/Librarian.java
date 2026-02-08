23f5a61719fd0170b0a3b395c673425a5bc66ee1
ZamJunio <samjunio0123@gmail.com>
2026-02-08 20:26:58 +0800

    Librarian.java

---
+++ librarySystem/Librarian.java
@@ -0,0 +1,18 @@
+package librarySystem;
+
+public class Librarian {
+
+    private String librarianName;
+
+    public Librarian(String librarianName) {
+        this.librarianName = librarianName;
+    }
+
+    public String getName() {
+        return librarianName;
+    }
+
+    public String getIdentity() {
+        return "Librarian: Assists borrowers";
+    }
+}
